57e42abc7b1cf8fb3a31857a81eead5b138588f0
haotongwang <50545432+haotongwang@users.noreply.github.com>
2019-05-22 12:55:08 +1000

    Made circle

--- bouncing-ball-script.js
+++ bouncing-ball-script.js
@@ -1,1 +1,8 @@
+const c = document.getElementById('myCanvas');
+const ctx = c.getContext('2d');
+const width = Number(c.width);
+const height = Number(c.height);
 
+ctx.beginPath();
+ctx.arc(150, 100, 20, 0, 2*Math.PI);
+ctx.stroke();
