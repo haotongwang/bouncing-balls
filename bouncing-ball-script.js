6671031f4331c33c8c994828146c407e4f6b4b83
haotongwang <50545432+haotongwang@users.noreply.github.com>
2019-05-22 16:21:06 +1000

    Made ball move

--- bouncing-ball-script.js
+++ bouncing-ball-script.js
@@ -1,8 +1,43 @@
-const c = document.getElementById('myCanvas');
-const ctx = c.getContext('2d');
-const width = Number(c.width);
-const height = Number(c.height);
+const canvas = document.getElementById('myCanvas');
+const ctx = canvas.getContext('2d');
 
-ctx.beginPath();
-ctx.arc(150, 100, 20, 0, 2*Math.PI);
-ctx.stroke();
+
+
+class Blob {
+  constructor(colour, size, xSpeed, ySpeed){
+    this.x = Math.random() * canvas.width;
+    this.y = Math.random() * canvas.height;
+    this.colour = colour;
+    this.size = size;
+    this.xSpeed = xSpeed;
+    this.ySpeed = ySpeed;
+
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.size, 0, 2*Math.PI);
+    ctx.fillStyle = this.colour;
+    ctx.fill();
+    ctx.stroke();
+  }
+
+  move(){
+    if (this.x >= canvas.width || this.x <= 0){
+      this.xSpeed = -1 *this.xSpeed;
+    }
+    if (this.y >= canvas.height || this.y <= 0){
+      this.ySpeed = -1*this.ySpeed;
+    }
+
+    this.x += this.xSpeed;
+    this.y += this.ySpeed;
+
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.size, 0, 2*Math.PI);
+    ctx.fillStyle = this.colour;
+    ctx.fill();
+    ctx.stroke();
+  }
+}
+
+const newBlob = new Blob('green', 20, 1, 1);
+
+setInterval(function(){newBlob.move()}, 10);
